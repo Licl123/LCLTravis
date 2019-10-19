5b8c118743c255363c4878062d7c21108c1fc5e4
licl <a1@licldeMacBook-Pro.local>
2019-10-19 21:54:10 +0800

    submit demo

---
+++ 005---LCLTravis/005---LCLTravis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  005---LCLTravis
+//
+//  Created by licl on 2019/10/19.
+//  Copyright © 2019 lcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 005---LCLTravis/005---LCLTravis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  005---LCLTravis
+//
+//  Created by licl on 2019/10/19.
+//  Copyright © 2019 lcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
